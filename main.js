cd65041f6bace8b043582fc16800945505dd4bc6
Leenqq <eugeneverbitskiy84@gmail.com>
2024-05-14 19:50:11 +0300

    implement surface rotation based on hardware accelerometer sensor readings

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ let webCameraTexture;
 let webCameraVideo;
 let webCamera;
 
+let sensor;
+let reading = { x: 0, y: 0, z: 0 };
+
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
@@ -25,7 +28,7 @@ function Model(name) {
     this.iTextCoordBuffer = gl.createBuffer();
     this.count = 0;
 
-    this.BufferData = function(vertices, textCoords) {
+    this.BufferData = function (vertices, textCoords) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
@@ -33,10 +36,10 @@ function Model(name) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iTextCoordBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textCoords), gl.STREAM_DRAW);
 
-        this.count = vertices.length/3;
+        this.count = vertices.length / 3;
     }
 
-    this.Draw = function() {
+    this.Draw = function () {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
@@ -45,7 +48,7 @@ function Model(name) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iTextCoordBuffer);
         gl.vertexAttribPointer(shProgram.iAttribTextCoord, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribTextCoord);
-   
+
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
     }
 }
@@ -70,7 +73,7 @@ function ShaderProgram(name, program) {
 
     this.iTMU = -1;
 
-    this.Use = function() {
+    this.Use = function () {
         gl.useProgram(this.prog);
     }
 }
@@ -80,19 +83,24 @@ function ShaderProgram(name, program) {
  * (Note that the use of the above drawPrimitive function is not an efficient
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
-function draw() { 
-    gl.clearColor(0,0,0,1);
+function draw() {
+    gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     /* Set the values of the projection transformation */
     let projection = m4.orthographic(0, 1, 0, 1, -1, 1);
-    
+
     /* Get the view matrix from the SimpleRotator object.*/
+
     let modelView = spaceball.getViewMatrix();
 
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0);
+    let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0);
 
-    let matAccum = m4.multiply(rotateToPointZero, modelView );
+    let matAccel = getAccelMatrix();
+    let matTr = m4.translation(0, 0, -1);
+    modelView = m4.multiply(matAccel, matTr);
+
+    let matAccum = m4.multiply(rotateToPointZero, modelView);
 
     let noRot = m4.multiply(rotateToPointZero, [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
 
@@ -157,7 +165,7 @@ function CreateSurfaceData() {
             vertexList.push(v1.x, v1.y, v1.z);
             vertexList.push(v2.x, v2.y, v2.z);
             vertexList.push(v3.x, v3.y, v3.z);
-            
+
             vertexList.push(v2.x, v2.y, v2.z);
             vertexList.push(v4.x, v4.y, v4.z);
             vertexList.push(v3.x, v3.y, v3.z);
@@ -170,7 +178,7 @@ function CreateSurfaceData() {
             textCoordList.push(t1.r, t1.theta);
             textCoordList.push(t2.r, t2.theta);
             textCoordList.push(t3.r, t3.theta);
-            
+
             textCoordList.push(t2.r, t2.theta);
             textCoordList.push(t4.r, t4.theta);
             textCoordList.push(t3.r, t3.theta);
@@ -182,10 +190,10 @@ function CreateSurfaceData() {
 
 function CalculateTextCoord(r, theta) {
 
-    r = (r - 0.25)/(maxR - 0.25);
-    theta = theta / 2*Math.PI;
+    r = (r - 0.25) / (maxR - 0.25);
+    theta = theta / 2 * Math.PI;
 
-    return {r, theta};
+    return { r, theta };
 }
 
 function equations(r, theta) {
@@ -193,33 +201,33 @@ function equations(r, theta) {
     let y = -(Math.sin(theta) / (2 * r)) + (Math.pow(r, 3) * Math.sin(3 * theta) / 6);
     let z = r * Math.cos(theta);
 
-    return { x: x, y: y, z: z}
+    return { x: x, y: y, z: z }
 }
 
-function applyLeftFrustum (stereoCamera) {
+function applyLeftFrustum(stereoCamera) {
     let { eyeSeparation, convergence, aspectRatio, fov, near, far } = stereoCamera;
     let top = near * Math.tan(fov / 2);
     let bottom = -top;
-  
+
     let a = aspectRatio * Math.tan(fov / 2) * convergence;
     let b = a - eyeSeparation / 2;
     let c = a + eyeSeparation / 2;
-  
+
     let left = (-b * near) / convergence;
     let right = (c * near) / convergence;
-  
+
     return m4.orthographic(left, right, bottom, top, near, far);
 }
 
-  function applyRightFrustum (stereoCamera) {
+function applyRightFrustum(stereoCamera) {
     let { eyeSeparation, convergence, aspectRatio, fov, near, far } = stereoCamera;
     let top = near * Math.tan(fov / 2);
     let bottom = -top;
-  
+
     let a = aspectRatio * Math.tan(fov / 2) * convergence;
     let b = a - eyeSeparation / 2;
     let c = a + eyeSeparation / 2;
-  
+
     let left = (-c * near) / convergence;
     let right = (b * near) / convergence;
 
@@ -275,25 +283,42 @@ function LoadWebCameraTexture() {
 
 function getWebCamera() {
     return new Promise((resolve) =>
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: false })
-        .then((s) => resolve(s))
+        navigator.mediaDevices
+            .getUserMedia({ video: true, audio: false })
+            .then((s) => resolve(s))
     );
-  };
+};
+
+function getAccelMatrix() {
+    let matAccel1 = m4.axisRotation([1, 0, 0], 0.5 * Math.PI * reading.y * 0.1)
+    let matAccel2 = m4.axisRotation([0, 1, 0], -0.5 * Math.PI * reading.x * 0.1)
+    let matAccel = m4.multiply(matAccel1, matAccel2);
+    return matAccel;
+}
+
+function readAccelerometer() {
+    sensor = new Accelerometer({ frequency: 60 });
+    sensor.addEventListener("reading", () => {
+        reading.x = sensor.x
+        reading.y = sensor.y
+        reading.z = sensor.z
+    });
+    sensor.start();
+}
 
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
+    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
     shProgram = new ShaderProgram('Basic', prog);
     shProgram.Use();
 
-    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iModelViewMatrix           = gl.getUniformLocation(prog, "ModelViewMatrix");
-    shProgram.iProjectionMatrix          = gl.getUniformLocation(prog, "ProjectionMatrix");
-    shProgram.iAttribTextCoord           = gl.getAttribLocation(prog, "textCoord");
-    shProgram.iTMU                       = gl.getUniformLocation(prog, "tmu");
+    shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
+    shProgram.iProjectionMatrix = gl.getUniformLocation(prog, "ProjectionMatrix");
+    shProgram.iAttribTextCoord = gl.getAttribLocation(prog, "textCoord");
+    shProgram.iTMU = gl.getUniformLocation(prog, "tmu");
 
     surface = new Model('Surface');
     let data = CreateSurfaceData();
@@ -321,24 +346,24 @@ function initGL() {
  * source code for the vertex shader and for the fragment shader.
  */
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader( gl.VERTEX_SHADER );
-    gl.shaderSource(vsh,vShader);
+    let vsh = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vsh, vShader);
     gl.compileShader(vsh);
-    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
+    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
         throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-     }
-    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
+    }
+    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
-    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
-       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
-    gl.attachShader(prog,vsh);
+    gl.attachShader(prog, vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
-    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
-       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+        throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
@@ -348,11 +373,12 @@ function createProgram(gl, vShader, fShader) {
  * initialization function that will be called when the page has loaded
  */
 function init() {
+    readAccelerometer();
     let canvas;
     try {
         canvas = document.getElementById("webglcanvas");
         gl = canvas.getContext("webgl");
-        if ( ! gl ) {
+        if (!gl) {
             throw "Browser does not support WebGL";
         }
 
